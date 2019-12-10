f601e0ec27b67d1b98f63f433d15e23e79c9dd7a
Alanna <alanna.noguchi@students.makeschool.com>
2019-12-09 16:32:55 -0800

    Updated style

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import sample
 import histogram
 
@@ -6,8 +6,22 @@ import histogram
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def hello_world():
+#     with open("sample.txt", "r") as data:
+#         histo = histogram.histogram_dictionary(data)
+#     return sample.random_word(histo)
+
+
 @app.route('/')
-def hello_world():
+def index():
+    """Return homepage."""
     with open("sample.txt", "r") as data:
         histo = histogram.histogram_dictionary(data)
-    return sample.random_word(histo)
+        word = sample.random_word(histo)
+    return render_template('index.html', word = word)
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
