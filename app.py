c11fc1b957aed333923fef530af96f9f8b208b20
Alanna <alanna.noguchi@students.makeschool.com>
2019-12-12 13:53:30 -0800

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import sample
 import histogram, markovchain
 from dictogram import Dictogram
-from markovchain import random_walk, markovhistogram
+from markovchain import random_walk_second_order, markov_second_order
 
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ app = Flask(__name__)
 #         histo = histogram.histogram_dictionary(data)
 #     return sample.random_word(histo)
 
-corpus = "That fantasy of what your life would be. White dress, Prince Charming, who'd carry you away to a castle on a hill. You\'d lie in bed at night and close your eyes, and you had complete and utter faith.[sighs.]Eight hours, 1 6 ounces of chocolate and 32 cupcakes, and they still don\'t taste right. No, these are good. Martha Stewart would be proud. Yeah, look where it got her."
+corpus = "That fantasy of what your life would be. White dress, Prince Charming, who'd carry you away to a castle on a hill. You\'d lie in bed at night and close your eyes, and you had complete and utter faith. Eight hours, 1 6 ounces of chocolate and 32 cupcakes, and they still don\'t taste right. No, these are good. Martha Stewart would be proud. Yeah, look where it got her."
 
 words = corpus.split() #split the corpus into individual words
 # pairs = [words[i]+' '+words[i+1] for i in range(len(words)-1)]
@@ -27,8 +27,8 @@ def index():
     # with open("sample.txt", "r") as data:
     #     histo = histogram.histogram_dictionary(data)
     #     word = sample.random_word(histo)
-    markov = markovhistogram(words)
-    random_sent = random_walk(words, markov)
+    markov =  markov_second_order(words)
+    random_sent = random_walk_second_order(markov)
  
     return render_template('index.html', random_sent = random_sent)
 
