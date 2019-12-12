42f9d86c6238ca492fefe58294e841934cb4a236
Alanna <alanna.noguchi@students.makeschool.com>
2019-12-11 16:48:14 -0800

    Update marcov

--- marcovchain.py
+++ marcovchain.py
@@ -6,21 +6,29 @@ words = corpus.split() #split the corpus into individual words
 # print(pairs)
 
 
-for i in range(len(words) -1):
-    first_word = words[i]
-    second_word = words[i+1]
-    pairs = (first_word, second_word)
-    # print(pairs)
+def markovhistogram(words):
+    """Create a histogram for a markov chain"""
     big_dict ={}
-    # marcov_dict = {}  # can't use a regular dict
-    marcov_dict = Dictogram()  # to use a dictogram method (add_count), we must first establish a Dictogram() object
-    marcov_dict.add_count(second_word)
-    # print(marcov_dict)
 
-    big_dict[first_word] = marcov_dict
-    if first_word not in big_dict.keys():
-        big_dict.get(first_word).add_count(second_word)
-    else:
+    for i in range(len(words) -1):
+        first_word = words[i]
+        second_word = words[i+1]
+        # pairs = (first_word, second_word)
+        # print(pairs)
+
+        # big_dict[first_word] = marcov_dict
+        if first_word not in big_dict.keys():
+            histo = []
+            big_dict[first_word] = histo
+            
         big_dict[first_word].append(second_word)
-    print(big_dict)
+         
+    values = big_dict.items()
+    for key, value in values:
+        # marcov_dict = {}  # can't use a regular dict
+        big_dict[key] = Dictogram(value)  # to use a dictogram method (add_count), we must first establish a Dictogram() object
+        # marcov_dict.add_count(second_word)
+        # print(marcov_dict)
+    return big_dict
 
+print(markovhistogram(words))
